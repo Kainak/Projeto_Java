c102952dcdf6a7377546ea861dfba08cca961209
Rudson-50 <76129291+Rudson-50@users.noreply.github.com>
2023-11-10 18:38:01 -0300

    adicionar usuario

--- src/application/Interfaces/UsuariosInterface/AdicionarUsuario.java
+++ src/application/Interfaces/UsuariosInterface/AdicionarUsuario.java
@@ -1,11 +1,55 @@
 package application.Interfaces.UsuariosInterface;
 
-import javax.swing.*;
+import model.dao.UsuarioDao;
+import model.dao.impl.UsuarioDaoJDBC;
+import model.entities.Usuario;
+import db.DB;
 
-public class AdicionarUsuario {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdicionarUsuario extends JFrame {
     private JTextField nomeUsuario;
     private JTextField telefoneUsuario;
     private JButton salvarButton;
     private JButton voltarButton;
     private JPanel adicionarUsuario;
+
+    private UsuarioDao usuarioDAO;
+
+    public AdicionarUsuario() {
+        setTitle("Adicionar Usuário");
+        setSize(350, 350);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setContentPane(adicionarUsuario);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        usuarioDAO = new UsuarioDaoJDBC(DB.getConnection());
+
+        salvarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                salvarUsuario();
+            }
+        });
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new FormularioUsuarios();
+                dispose();
+            }
+        });
+    }
+
+    private void salvarUsuario() {
+        String nome = nomeUsuario.getText();
+        String telefone = telefoneUsuario.getText();
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail(nome);
+        usuario.setPassword(telefone);
+
+        usuarioDAO.insert(usuario);
+    }
 }
