2557e4caa1ec387a6c2936858e62623cb6671fb6
Vinicius Eduardo Agnes <114628323+ViniciusAgnes@users.noreply.github.com>
2023-11-29 13:58:00 -0300

    Deletar e Atualizar

--- src/application/UsuariosInterface/AtualizarUsuario.java
+++ src/application/UsuariosInterface/AtualizarUsuario.java
@@ -1,4 +1,59 @@
 package application.UsuariosInterface;
 
+import db.DB;
+import model.dao.impl.UsuarioDao;
+import model.dao.impl.UsuarioDaoJDBC;
+import model.entities.Usuario;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class AtualizarUsuario {
+    private JTextField getAtualizaçãoDoUsuarioTextFieldUsuario;
+    private JButton voltarButton;
+    private JFormattedTextField ATUALIZAÇÃODEUSUARIOFormattedTextField;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton atualizar;
+    private JPasswordField passwordField3;
+
+    private UsuarioDao usuarioDAO;
+
+    private void createUIComponents() {
+    }
+    public AtualizarUsuario() {
+        setTitle("ATUALIZAR USUARIO");
+        setSize(350, 350);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setContentPane(AtualizarUsuario);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        usuarioDAO = new UsuarioDaoJDBC(DB.getConnection());
+
+        salvarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                salvarUsuario();
+            }
+        });
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ListaUsuarios();
+                dispose();
+            }
+        });
+    }
+
+    private void salvarUsuario() {
+        String nome = nomeUsuario.getText();
+        String telefone = telefoneUsuario.getText();
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail(nome);
+        usuario.setPassword(telefone);
+
+        usuarioDAO.insert(usuario);
+    }
 }
--- src/application/UsuariosInterface/DeletarUsuario.java
+++ src/application/UsuariosInterface/DeletarUsuario.java
@@ -1,4 +1,56 @@
 package application.UsuariosInterface;
 
+import db.DB;
+import model.dao.impl.UsuarioDaoJDBC;
+import model.entities.Usuario;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class DeletarUsuario {
+
+    private JTextField DELETARUSUARIOTextField;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton deletarButton;
+    private JButton voltarButton;
+}
+
+    private void createUIComponents() {
+    }
+    public DeletarUsuario() {
+        setTitle("DELETAR USUARIO");
+        setSize(350, 350);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setContentPane(DeletarUsuario);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        usuarioDAO = new UsuarioDaoJDBC(DB.getConnection());
+
+        deletarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                deleteById();
+            }
+        });
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ListaUsuarios();
+                dispose();
+            }
+        });
+    }
+
+    private void salvarUsuario() {
+        String nome = nomeUsuario.getText();
+        String telefone = telefoneUsuario.getText();
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail(nome);
+        usuario.setPassword(telefone);
+
+        usuarioDAO.insert(usuario);
+    }
 }
